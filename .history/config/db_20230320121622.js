86cd3d21d07fd6dc888720bb26ad31594c32ce21
rizwan4323 <rizwanzafar4323@gmail.com>
2023-03-20 12:17:02 +0500

    manully db path added in db.js

--- .history/config/db_20230320121622.js
+++ .history/config/db_20230320121622.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://rizwanzafar4323:zafar4323@ac-j2gridb-shard-00-00.9wsxerr.mongodb.net:27017,ac-j2gridb-shard-00-01.9wsxerr.mongodb.net:27017,ac-j2gridb-shard-00-02.9wsxerr.mongodb.net:27017/?ssl=true&replicaSet=atlas-5fvvuv-shard-0&authSource=admin&retryWrites=true&w=majority", {
+    await mongoose.connect("", {
       useFindAndModify: false,
       useNewUrlParser: true,
       useUnifiedTopology: true,
