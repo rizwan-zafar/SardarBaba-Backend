5650e94375d95a7c3881399c2abd533c51270959
rizwan4323 <rizwanzafar4323@gmail.com>
2023-03-20 12:38:58 +0500

    .env file renamed to config.env

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
+require('dotenv').config({path:"../config.env"});
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://rizwanzafar4323:zafar4323@ac-j2gridb-shard-00-00.9wsxerr.mongodb.net:27017,ac-j2gridb-shard-00-01.9wsxerr.mongodb.net:27017,ac-j2gridb-shard-00-02.9wsxerr.mongodb.net:27017/?ssl=true&replicaSet=atlas-5fvvuv-shard-0&authSource=admin&retryWrites=true&w=majority", {
+    await mongoose.connect( process.env.MONGO_URI, {
       useFindAndModify: false,
       useNewUrlParser: true,
       useUnifiedTopology: true,
